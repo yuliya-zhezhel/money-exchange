f16a23e6b6e8f5598257e8da62f37d7f76dea5bc
yuliya-zhezhel <yuliya.zhezhel@gmail.com>
2019-03-21 19:18:11 +0300

    decide task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    var coins ={
+        'H' : 50,
+        'Q' : 25,
+        'D' : 10,
+        'N' : 5,
+        'P' : 1
+}
+var result = {};
+if (currency <=0){
+// result.error = 'You are rich, my friend! We don\'t have so much coins for exchange';
+}else if(currency >= 10000){
+    result.error = 'You are rich, my friend! We don\'t have so much coins for exchange';
+}
+else{
+        for(key in coins){
+            var currentNumOfCurrrentCoin = 0;
+            currentNumOfCurrrentCoin = currency/coins[key];
+        if(currentNumOfCurrrentCoin >= 1){                  
+                result[key] = currentNumOfCurrrentCoin-(currentNumOfCurrrentCoin%1);
+
+        } 
+        if(currentNumOfCurrrentCoin%1==0) break;       
+        currency= currency-(currentNumOfCurrrentCoin-(currentNumOfCurrrentCoin%1))*coins[key];
+        }
+        
+}
+return result;
 }
